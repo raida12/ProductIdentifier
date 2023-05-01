190a4009878a672c03f2faad9a1a6e6c4bcc193e
raida12 <108494032+raida12@users.noreply.github.com>
2023-05-01 19:09:32 +0600

    refactoring

--- tests/tests_models.py
+++ tests/tests_models.py
@@ -6,18 +6,18 @@ class TestModels(TestCase):
     
     def setUp(self):
         
-        self.user2 = User.objects.create_user('raida123','raida@gmail.com','1234')
+        self.user2 = User.objects.create_user('raida1','raida@gmail.com','1234')
         
         self.customer2 = Customer.objects.create(
             user = self.user2,
-            name = 'raida123',
-            phone = '01710899896',
+            name = 'raida1',
+            phone = '01710899999',
             email = 'raida123@gmail.com'
         )
         
     
     def test_customer_model_creation(self):
-        self.assertEquals(self.customer2.user.username,'raida123')
+        self.assertEquals(self.customer2.user.username,'raida1')
         
     def test_product_model_creation(self):
         
@@ -26,4 +26,4 @@ class TestModels(TestCase):
             name = 'new product',
         )
         self.assertEquals(product.name,'new product')
-        self.assertEquals(product.customer.name,'raida123')
+        self.assertEquals(product.customer.name,'raida1')
